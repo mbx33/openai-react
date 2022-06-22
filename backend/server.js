fbfab395919849d69ce4072c6323a03b69fe12b3
Matt Baxter <mbx33@hotmail.com>
2022-06-21 19:40:48 -0600

    correcting the file for build

--- backend/server.js
+++ backend/server.js
@@ -17,14 +17,13 @@ app.use(express.static(path.join(__dirname, 'frontend/dist')));
 
 if (process.env.NODE_ENV === 'production') {
 	app.use(express.static(path.join(__dirname, 'frontend/dist')));
+	app.get('*', (req, res) => {
+		res.sendFile(path.join((__dirname = 'frontend/dist/index.html')));
+	});
 }
 
 app.get('*', (req, res) => {
-	res.sendFile(path.join((__dirname = 'client/build/index.html')));
-});
-
-app.get('*', (req, res) => {
-	res.sendFile(path.join(__dirname + '/frontend/src/index.html'));
+	res.sendFile(path.join(__dirname + '/frontend/index.html'));
 });
 
 app.post('/api/question', async (req, res) => {
