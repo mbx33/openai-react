d19dd8c9ef4078bc79f4f909285906cd78f077a7
Matt Baxter <mbx33@hotmail.com>
2022-06-21 19:37:17 -0600

    added get path for frontend file

--- backend/server.js
+++ backend/server.js
@@ -18,7 +18,10 @@ app.use(express.static(path.join(__dirname, 'frontend/dist')));
 if (process.env.NODE_ENV === 'production') {
 	app.use(express.static(path.join(__dirname, 'frontend/dist')));
 }
-//  app.get('*', (req, res) => {    res.sendfile(path.join(__dirname = 'client/build/index.html'));  })}
+
+app.get('*', (req, res) => {
+	res.sendFile(path.join((__dirname = 'client/build/index.html')));
+});
 
 app.get('*', (req, res) => {
 	res.sendFile(path.join(__dirname + '/frontend/src/index.html'));
